f4a0ec51d2f1fe9a35b54f1b8a04ec769b362e38
= <=>
2025-01-18 21:34:42 +0200

    .

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter , Routes, Route } from "react-router-dom";
 import Home from './Home'
 import About from './About'
 
@@ -6,10 +6,13 @@ function App() {
 
   return (
    <div>
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
         </Routes>
+      </BrowserRouter>
+        
    </div>
   );
 }
